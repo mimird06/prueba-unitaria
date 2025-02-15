3c834753f5aca9df79719cc7dde81b3d2181b0cd
mimird06 <thammyfva@hotmail.com>
2025-02-14 20:32:46 -0400

    Cambiando nombre de las pruebas unitarias para confirmar su buen uso

--- test_app.py
+++ test_app.py
@@ -6,8 +6,8 @@ def app():
     return App()
 
 def test_CrearUsuarioFuncional(app):
-    usuario = app.CrearUsuario("Thammy", "thammy@gmail.com")
-    assert usuario["nombre"] == "Thammy"
+    usuario = app.CrearUsuario("Franceily", "thammy@gmail.com")
+    assert usuario["nombre"] == "Franceily"
     assert usuario["correo"] == "thammy@gmail.com"
 
 def test_CrearUsuarioVacio(app):
@@ -16,14 +16,14 @@ def test_CrearUsuarioVacio(app):
 
 def test_CrearCorreoInvalido(app):
     with pytest.raises(ValueError, match="El correo no es válido"):
-        app.CrearUsuario("Thammy", "correo_invalido")
+        app.CrearUsuario("Franceily", "correo_invalido")
 
 def test_InicioSesionFuncional(app):
-    usuarios = [{"nombre": "Thammy", "password": "1234"}]
-    mensaje = app.IniciarSesion(usuarios, "Thammy", "1234")
+    usuarios = [{"nombre": "Franceily", "password": "1234"}]
+    mensaje = app.IniciarSesion(usuarios, "Franceily", "1234")
     assert mensaje == "Inicio de sesión exitoso"
 
 def test_InicioSesionIncorrecto(app):
-    usuarios = [{"nombre": "Thammy", "password": "1234"}]
+    usuarios = [{"nombre": "Franceily", "password": "1234"}]
     with pytest.raises(ValueError, match="Credenciales incorrectas"):
-        app.IniciarSesion(usuarios, "Thammy", "wrongpass")
+        app.IniciarSesion(usuarios, "Franceily", "wrongpass")
